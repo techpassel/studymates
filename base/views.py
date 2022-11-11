5ee23e57fa7251611a24c69f2379cd93e15a837a
techpassel <techpasselorg@gmail.com>
2022-11-11 12:09:10 +0530

    Added templates in the projects and rendered some of them

--- base/views.py
+++ base/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home page")
+    # return HttpResponse("Home page")
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse("Room page")
+    # return HttpResponse("Room page")
+    return render(request, 'room.html')
